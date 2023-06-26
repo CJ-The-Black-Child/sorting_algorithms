3eef4b1179134c20ead3d37aee78e9516579ff5f
Congo Justus Migue <congomigue@gmail.com>
2023-06-26 15:57:55 +0000

    #3 fixed betty coding syntax

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -11,18 +11,18 @@ void quick_sort(int *array, size_t size)
 {
 	if (array == NULL || size < 2)
 		return;
-
-	quicksort_recursive(array, 0, size - 1);
+	lomuto_sort(array, 0, size - 1, size);
 }
 
 /**
- * quicksort_recursive - recursive function to perform quick sort
+ * lomuto_sort - performs quicksort using lomuto partition scheme
  * @array: Pointer to the array to be sorted
  * @low: starting index of the partition to be sorted.
  * @high: ending index of the partition to be sorted.
+ * @size: size of the array
  */
 
-void quicksort_recursive(int *array, int low, int high)
+void lomuto_sort(int *array, int low, int high, size_t size)
 {
 	int partition;
 
@@ -30,8 +30,8 @@ void quicksort_recursive(int *array, int low, int high)
 	{
 		partition = lomuto_partition(array, low, high);
 		print_array(array, high - low + 1);
-		quicksort_recursive(array, low, partition - 1);
-		quicksort_recursive(array, partition + 1, high);
+		lomuto_sort(array, low, partition - 1, size);
+		lomuto_sort(array, partition + 1, high, size);
 	}
 }
 
@@ -49,30 +49,30 @@ int lomuto_partition(int *array, int low, int high)
 	int pivot, i, j, temp;
 
 	pivot = array[high];
-	i = low - 1;
+	i = low;
 
 	for (j = low; j <= high - 1; j++)
 	{
 		if (array[j] < pivot)
 		{
-			i++;
 			if (i != j)
 			{
 				temp = array[i];
 				array[i] = array[j];
 				array[j] = temp;
-				print_array(array, high - low + 1);
+				print_array(array, (size_t)(high - low + 1));
 			}
+			i++;
 		}
 	}
 
-	if (array[i + 1] != array[high])
+	if (array[i] != array[high])
 	{
-		temp = array[i + 1];
-		array[i + 1] = array[high];
+		temp = array[i];
+		array[i] = array[high];
 		array[high] = temp;
-		print_array(array, high - low + 1);
+		print_array(array, (size_t)(high - low + 1));
 	}
 
-	return (i + 1);
+	return (i);
 }
